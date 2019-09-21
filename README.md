1caecd5b75e9681b8203fdc44305a49a4ca4b4fb
wu.teng <wu.teng@outlook.com>
2019-09-21 20:16:26 +0800

    update reademe

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # IniParser
 ini style file parser for node
+
+### install
+```shell
+npm i iniparserjs
+```
+
+### usage
+`config.ini` file like:
+```shell
+[global]
+key1=false
+
+[section1]
+key1=value1
+
+[section2]
+key1=value1
+key2=value2
+```
+
+with IniParser:
+```js
+var IniParser = require("iniparserjs")
+
+var config = new IniParser("test.ini", "UTF8")
+
+// get all sections
+var sections = config.sections()
+
+//get all keys of section1
+var keysOfSection1 = config.keysOfSection("section1")
+
+//get value by key and section
+var key1InSection2 = config.get("section2", "key1")
+
+//reset a key-value
+config.set("section2", "key2", "newValue2")
+//set a new key-value
+config.set("section3", "key1", "value1")
+
+//resave
+var opt = {
+    path: "new_config.ini",
+    encoding: "UTF8",  // default is "UTF8"
+    toDelimiter: "="  // default is "="
+}
+config.save(opt)
+
+//print
+console.log("sections = ", sections)
+console.log("keysOfSection1 = ", keysOfSection1)
+console.log("key1InSection2 = ", key1InSection2)
+```
+
+output:
+```shell
+sections =  [ 'global', 'section1', 'section2' ]
+keysOfSection1 =  [ 'key1' ]
+key1InSection2 =  2
+```
+
+`new_config.ini` file will like:
+```shell
+[global]
+key1=false
+
+[section1]
+key1=value1
+
+[section2]
+key1=2
+key2=newValue2
+
+[section3]
+key1=value1
+```
+
--- test.js
+++ test.js
@@ -1,18 +1,30 @@
 var IniParser = require("./index")
 
-var config = new IniParser("config_test.ini")
-// console.log(typeof IniParser)
-console.log('sections', config.sections())
-// console.log('get name in section1'. config.get("section1", "name"))
-console.log('get keys of section1', config.keysOfSection("section1"))
-console.log('set some key-value')
-config.set("globalKey", 'globalValue')
-config.set("section1", "name", "newValue")
-config.set("section3", "name", "section3")
-console.log('restore')
-config.save({
-    path: "new_config_test.ini",
-    encoding: "UTF8",
-    toDelimiter: "="
-})
-console.log(config.configs)
+var config = new IniParser("test.ini", "UTF8")
+
+// get all sections
+var sections = config.sections()
+
+//get all keys of section1
+var keysOfSection1 = config.keysOfSection("section1")
+
+//get value by key and section
+var key1InSection2 = config.get("section2", "key1")
+
+//reset a key-value
+config.set("section2", "key2", "newValue2")
+//set a new key-value
+config.set("section3", "key1", "value1")
+
+//resave
+var opt = {
+    path: "new_config.ini",
+    encoding: "UTF8",  // default is "UTF8"
+    toDelimiter: "="  // default is "="
+}
+config.save(opt)
+
+//print
+console.log("sections = ", sections)
+console.log("keysOfSection1 = ", keysOfSection1)
+console.log("key1InSection2 = ", key1InSection2)
